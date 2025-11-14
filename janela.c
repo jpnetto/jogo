8c161e072c2ea24101c543834ef3da9b2562cd49
Bernardo Resende <resendebr21@gmail.com>
2025-11-14 15:00:49 -0300

    Update janela.c

--- janela.c
+++ janela.c
@@ -6,6 +6,12 @@
 #include <time.h>
 #include "janela.h"
 
+void Startgame(Jogo*j){
+    j->contador = 0;
+    j->largura = 660;
+    j->altura = 660;
+}
+
 void Reset_score(Jogo* j){
     j->contador = 0;
 }
@@ -14,46 +20,132 @@ void Increase_score(Jogo* j){
     j->contador++;
 }
 
-void Size_map(Jogo* j){
-    while (!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Aperte A para jogar no mapa Pequeno", 15, 100, 30, WHITE);
-        DrawText("Aperte B para jogar no mapa Médio", 15, 200, 30, WHITE);
-        DrawText("Aperte C para jogar no mapa Grande", 15, 300, 30, WHITE);
-        if(IsKeyPressed(KEY_A)){
-            j->altura = 500;
-            j->largura = 500;
-            EndDrawing();
-            break;
-        } else if(IsKeyPressed(KEY_B)){
-            j->altura = 660;
-            j->largura = 660;
-            EndDrawing();
-            break;
-        } else if(IsKeyPressed(KEY_C)){
-            j->altura = 820;
-            j->largura = 820;
-            EndDrawing();
-            break;
-        }
-        EndDrawing();
-    }
-    CloseWindow();
-}
-
 void Death_message(Jogo* j){
     if(j->largura == 500){
         DrawText("FIM DE JOGO!", 65, 120, 55, RED);
         DrawText(TextFormat("Sua Pontuação: %d", j->contador), 75, 240, 40, BLACK);
         DrawText("PRESSIONE ENTER PARA REINICIAR", 25, 350, 25, RED);
+        DrawText("PRESSIONE ESPAÇO", 120, 400, 25, BLACK);
+        DrawText("PARA VOLTAR AO MENU", 100, 425, 25, BLACK);
     } else if(j->largura == 660){
         DrawText("FIM DE JOGO!", 80, 160, 75, RED);
         DrawText(TextFormat("Sua Pontuação: %d", j->contador), 110, 280, 50, BLACK);
         DrawText("PRESSIONE ENTER PARA REINICIAR", 50, 420, 30, RED);
+        DrawText("PRESSIONE ESPAÇO", 160, 500, 30, BLACK);
+        DrawText("PARA VOLTAR AO MENU", 135, 530, 30, BLACK);
     } else{
         DrawText("FIM DE JOGO!", 120, 170, 85, RED);
         DrawText(TextFormat("Sua Pontuação: %d", j->contador), 160, 370, 55, BLACK);
-        DrawText("PRESSIONE ENTER PARA REINICIAR", 35, 560, 40, RED);
+        DrawText("PRESSIONE ENTER PARA REINICIAR", 35, 530, 40, RED);
+        DrawText("PRESSIONE ESPAÇO", 190, 620, 40, BLACK);
+        DrawText("PARA VOLTAR AO MENU", 160, 660, 40, BLACK);
     }
+}
+
+int Menu_screen(Jogo* j){
+    int cursor = 0;
+    int retorno = 1;
+    Rectangle selection;
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(LIME);
+        DrawText("Cobrinha", 210, 50, 50, BLACK);
+        DrawText("dos", 280, 115, 50, BLACK);
+        DrawText("BackyEndigans", 155, 180, 50, BLACK);
+        DrawText("Iniciar", 20, 440, 30, BLACK);
+        DrawText("Configurações de Mapa", 20, 500, 30, BLACK);
+        DrawText("Ranking", 20, 560, 30, BLACK);
+        
+        
+        if(IsKeyPressed(KEY_UP)){
+            if(cursor<=0)cursor = 0;
+            else cursor--;
+        };
+        if(IsKeyPressed(KEY_DOWN)){
+            if(cursor>1)cursor = 2;
+            else cursor++;
+        };
+        if(cursor <= 0){
+            Size_cursor(&selection, 20, 475, 90, 5);
+        } else if(cursor == 1){
+            Size_cursor(&selection, 20, 535, 355, 5);
+        } else if(cursor > 1){
+            Size_cursor(&selection, 20, 595, 120, 5);
+        }
+        if(IsKeyPressed(KEY_ENTER)){
+            if(cursor == 0){
+                retorno = 0;
+                break;
+            } else if(cursor == 1){
+                CloseWindow();
+                Size_map(j);
+                InitWindow(660, 660, " ");
+            } else{
+                retorno = 0;
+                break; 
+            } 
+            
+        }
+        DrawRectangleRec(selection, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
+    return retorno;    
+}
+
+
+
+
+void Size_map(Jogo* j){
+    InitWindow(660, 660, "Cobrinha dos BackyEndigans");
+    int cursor = 0;
+    Rectangle selection;
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Aperte ENTER na posicao desejada", 55, 100, 30, WHITE);
+        DrawText("Mapa Pequeno", 185, 200, 40, WHITE);
+        DrawText("Mapa Médio", 185, 350, 40, WHITE);
+        DrawText("Mapa Grande", 185, 500, 40, WHITE);
+        
+        if(IsKeyPressed(KEY_UP)){
+            if(cursor<=0)cursor = 0;
+            else cursor--;
+        };
+        if(IsKeyPressed(KEY_DOWN)){
+            if(cursor>1)cursor = 2;
+            else cursor++;
+        };
+        if(cursor <= 0){
+            Size_cursor(&selection, 180, 245, 300, 5);
+        } else if(cursor == 1){
+            Size_cursor(&selection, 180, 395, 300, 5);
+        } else if(cursor > 1){
+            Size_cursor(&selection, 180, 545, 300, 5);
+        }
+        if(IsKeyPressed(KEY_ENTER)){
+            if(cursor == 0){
+                j->altura = 500;
+                j->largura = 500;
+            } else if(cursor == 1){
+                j->altura = 660;
+                j->largura = 660;
+            } else{
+                j->altura = 820;
+                j->largura = 820;
+            }
+            EndDrawing();
+            break; 
+        }
+        DrawRectangleRec(selection, WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+}
+
+void Size_cursor(Rectangle* selection, int a, int b, int c, int d){
+    selection->x = a;
+    selection->y = b;
+    selection->width = c;
+    selection->height = d;
 }
