52dd1d5267a54663d400a9f4fb7fb3310de6274a
Sophia Santana Silva <sophiass.sjdr@gmail.com>
2025-12-07 09:10:18 -0300

    algumas modificações
    ainda mexendo nos obstáculos

--- obstaculos.h
+++ obstaculos.h
@@ -13,6 +13,7 @@ void IniciaObstaculosMapa2(Jogo* j);
 void DesenhaObstaculosMapa0(Jogo* j);
 void DesenhaObstaculosMapa1(Jogo* j);
 void DesenhaObstaculosMapa2(Jogo* j);
+void DesenhaTextura(Texture2D textura, Rectangle obstaculo);
 
 int ColisaoObstaculosMapa0(Jogo* j);
 int ColisaoObstaculosMapa1(Jogo* j);
